66830cf488276773e5ccb3c5f641a727de0cc0c2
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-09-11 11:33:23 -0600

    se creo el repositorio para producto

---
+++ src/main/java/org/esfe/JDMR11092024/Repositorios/IProductoRepository.java
@@ -0,0 +1,8 @@
+package org.esfe.JDMR11092024.Repositorios;
+
+import org.esfe.JDMR11092024.Models.ProductoJDMR;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IProductoRepository extends JpaRepository<ProductoJDMR, Integer>{
+
+}

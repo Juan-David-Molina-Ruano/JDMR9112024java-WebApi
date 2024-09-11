3b8405b82d7fd2b4a031a237ffcfadcbf3e11f20
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-09-11 10:28:14 -0600

    Inicializacion de proyecto de spring boot

---
+++ src/main/java/org/esfe/JDMR11092024/Jdmr11092024Application.java
@@ -0,0 +1,13 @@
+package org.esfe.JDMR11092024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdmr11092024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jdmr11092024Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/esfe/JDMR11092024/Jdmr11092024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esfe.JDMR11092024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdmr11092024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
